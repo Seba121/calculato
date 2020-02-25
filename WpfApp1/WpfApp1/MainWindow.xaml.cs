18d60044cd1ed6a8a59c5b07e97bff182d14df64
Student <Student@DESKTOP-GGOJP1D>
2020-02-25 13:29:34 +0100

    Finished button *

--- WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -42,5 +42,14 @@ namespace WpfApp1
 
             textBox2.Text = wynik.ToString();
         }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            int wynik = 0;
+
+            wynik = Int32.Parse(textBox.Text) * Int32.Parse(textBox1.Text);
+
+            textBox2.Text = wynik.ToString();
+        }
     }
 }
